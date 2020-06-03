8d95cf674ee548d7cff062a458c85f30352a0dc8
Luc Gotink <L.h.gotink@gmail.com>
2020-06-03 21:59:50 +0000

    made connection between getmovies and fullmovies

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import path
 if path.exists('env.py'):
     import env
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 import pymongo
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -35,12 +35,35 @@ def get_movies():
 def get_books():
     return render_template('getbooks.html', books=mongo.db.books.find())
 
-@app.route('/addbook',  methods=['POST'])
-def add_book():
-    book = mongo.db.books
-    book.insert_one(request.form.to_dict())
+@app.route('/full_movie/<movie_id>')
+def full_movie(movie_id):
+    return''
+
+@app.route('/full_book/<book_id>')
+def full_book(book_id):
+    this_book = mongo.db.books.find_one({'_id': ObjectId(book_id)})
+    return render_template('full_book.html', book = this_book)
+
+
+@app.route('/addmovie', methods=['GET','POST'])
+def add_movie():
+    movie = mongo.db.movies
+    movie.insert_one(request.form.to_dict())
     print(request.form.to_dict())
-    return redirect(url_for('get_books'))
+    return render_template('addmovie.html')
+
+@app.route('/addbook', methods=['GET', 'POST'])
+def add_book():
+    return render_template('addbook.html')
+
+@app.route('/insert_book', methods=['POST'])
+def insert_book():
+    books = mongo.db.books
+    books.insert_one(request.form.to_dict())
+    print(request.form.to_dict())
+    return redirect(url_for('full_book'))
+
+
 
 
 if __name__ == '__main__':
